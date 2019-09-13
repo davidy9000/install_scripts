dfcb9f618c257fdcccb9436121be813772938604
David Yuen <31258540+davidy9000@users.noreply.github.com>
2019-09-12 22:31:05 -0400

    Add Fedora Install Script

---
+++ f30_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Run as su
+
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y fedora-workstation-repositories rubygems
+
+dnf update -y
+
+#Some standard things
+dnf groupinstall -y "Development Tools" "Development Libraries" "C Development Tools and Libraries"
+dnf install -y nano vim wget rpm-build dnf-plugins-core flatpak neofetch gcc gcc-c++ python37 make git git-all patch fuse-exfat
+
+#VS Code Repo
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+#Negativo17 Spotify 
+dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
+
+dnf update -y
+#Packages that I use
+dnf install -y code spotify-client kdenlive vlc audacity obs-studio handbrake qbittorrent youtube-dl thunderbird redshift gimp ark steam
+dnf install -y ncurses-devel glibc-headers glibc-devel kernel-headers kernel-devel libgomp binutils dkms
+
+#Fonts
+dnf install -y mozilla-fira-mono-fonts.noarch mozilla-fira-sans-fonts.noarch
+
+#MS Core Fonts
+dnf install -y curl cabextract xorg-x11-font-utils fontconfig
+rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
+#My used python libraries
+#pip3 install numpy scipy folium pandas matplotlib django pymongo twisted scrapy tensorflow sympy flask opencv-contrib-python --user
+
+#rubygem package
+gem install lolcat
+
+#flatpak
+flatpak install flathub com.slack.Slack -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.sublimetext.three -y
