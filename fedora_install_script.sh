6a3a8d450194497896fb0879153f7103f43a4a1e
David Yuen <31258540+davidy9000@users.noreply.github.com>
2020-01-07 15:41:57 -0500

    Add PostgreSQL instructions

--- fedora_install_script.sh
+++ fedora_install_script.sh
@@ -82,3 +82,11 @@ flatpak install flathub com.sublimetext.three -y
 # sudo systemctl enable mongod.service
 # sudo systemctl start mongod.service
 ##############################################################
+# Install PostgreSQL 12
+# sudo dnf install https://download.postgresql.org/pub/repos/yum/reporpms/F-31-x86_64/pgdg-fedora-repo-latest.noarch.rpm
+# sudo dnf install postgresql12 postgresql12-server
+# Optionally initialize the database and enable automatic start: 
+# /usr/pgsql-12/bin/postgresql-12-setup initdb
+# sudo systemctl enable postgresql-12
+# sudo systemctl start postgresql-12
+##############################################################
